841fc27bed3ade528e2e9322bd7957ccde599ce8
Admin <admin@MacMini.local>
2017-04-12 12:14:38 +0200

    Animation effect + refactoring

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
+function removePlaying(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
 
-window.addEventListener('keydown', function(e) {
-const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-    if (audio !== null) {
-        audio.play();
-    }
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-});
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    key.classList.add('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removePlaying))
+window.addEventListener('keydown', playSound);
